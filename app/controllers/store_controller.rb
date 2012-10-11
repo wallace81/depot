3fc5a6c0b1248bb4a9a925454d006c77f4b70470
wallace81 <marcel5781@web.de>
2012-10-11 20:20:14 +0200

    task e

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,14 @@
+class StoreController < ApplicationController
+  def index
+    @products = Product.all
+    @cart = current_cart
+    @count = increment_count
+  end
+  
+  def increment_count
+  session[:counter] ||= 0
+  session[:counter] += 1
+end
+  
+  
+end
